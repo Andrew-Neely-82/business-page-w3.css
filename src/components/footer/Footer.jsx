737031c019c3575c567a78db398df09bae5904b4
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-05-13 18:05:05 -0500

    adding from another repo for hosting on github pages

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <div className="Footer__">
+      <span>
+        Powered by <a href="#">w3.css</a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
